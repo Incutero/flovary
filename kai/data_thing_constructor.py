0ad454486a9a5f181e346f567085d5600fabe207
Kai Xiao <kaixiao@RLE-EECS-MTL-DHCP-31-87.MIT.EDU>
2016-09-24 17:52:05 -0400

    Parser

--- kai/data_thing_constructor.py
+++ kai/data_thing_constructor.py
@@ -1,3 +1,23 @@
 import re
-m = re.search('(?<=abc)def', 'abcdef')
-m.group(0)
+
+# In python 2.x, all function names are comprised of digits, numbers, and underscores.
+test_file = open('../tests/006_ps5_test.py', 'r')
+python_code = test_file.read();
+
+# searchObj = re.findall( r'def [a-zA-z0-9_]*\((.*)\):', string, re.M|re.I)
+func_sigs = re.findall( r"def [a-zA-Z0-9_]*\(.*\):\n", python_code)
+
+for func_sig in func_sigs:
+	params = re.findall( r"(.*)", func_sig)
+	print func_sig
+	print params
+
+
+
+# if searchObj:
+#    print "search --> searchObj.group() : ", searchObj.group(0)
+#    print "search --> searchObj.group() : ", searchObj.group(1)
+# else:
+#    print "Nothing found!!"
+
+
