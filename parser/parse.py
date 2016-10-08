afe18e416737f2870c567691a220defd66eae84d
Michael Shum <michaelmshum@live.com>
2016-10-08 16:59:35 -0400

    moving tree walking to seperate helper, adding pyc to gitignore

--- parser/parse.py
+++ parser/parse.py
@@ -1,5 +1,6 @@
 import ast
 from collections import deque
+from ast_helper import dfs_walk
 
 class FuncCallVisitor(ast.NodeVisitor):
     def __init__(self):
@@ -22,42 +23,6 @@ class FuncCallVisitor(ast.NodeVisitor):
             self._name.appendleft(node.value.id)
         except AttributeError:
             self.generic_visit(node)
-
-def iter_fields(node):
-    """
-    Yield a tuple of ``(fieldname, value)`` for each field in ``node._fields``
-    that is present on *node*.
-    """
-    for field in node._fields:
-        try:
-            yield field, getattr(node, field)
-        except AttributeError:
-            pass
-
-def iter_child_nodes(node):
-    """
-    Yield all direct child nodes of *node*, that is, all fields that are nodes
-    and all items of fields that are lists of nodes.
-    """
-    child_nodes = []
-    for name, field in iter_fields(node):
-        if isinstance(field, ast.AST):
-            child_nodes.append(field)
-        elif isinstance(field, list):
-            for item in field:
-                if isinstance(item, ast.AST):
-                    child_nodes.append(item)
-    return child_nodes
-
-def dfs_walk(node):
-    todo = deque([node])
-    while todo:
-        node = todo.popleft()
-        child_nodes = iter_child_nodes(node)
-        child_nodes.reverse()
-        if child_nodes is not None:
-            todo.extendleft(child_nodes)
-        yield node
 
 def get_func_calls(tree):
     func_calls = []
