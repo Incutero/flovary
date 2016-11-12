f55073141bc4f3c600a685dfb7b50a7408eb41f4
Kai Xiao <kaixiao@nat-tvwna-inside-visitornet101-b-18780.princeton.org>
2016-11-12 15:01:29 -0500

    Created linkedlist for function calls

--- parser/classes/ASTFuncDef.py
+++ parser/classes/ASTFuncDef.py
@@ -6,22 +6,29 @@ from Call import Call
 class ASTFuncDef(object):
 
     def __init__(self, node):
+        self.node = node
         self.name = node.name
         self.calls = self.dfs_walk_get_calls(node)
 
     def __str__(self):
         print self.name
-
+ 
+    # Walks through function and returns linkedlist of calls.
     def dfs_walk_get_calls(self, node):
-        all_calls = []
         todo = deque([node])
+        latest_calls = None
+        first_call = None
         while todo:
             node = todo.popleft()
-            print node
             if isinstance(node, ast.Call):
-                all_calls.append(Call(node))
+                current_call = Call(node, latest_calls)
+                if first_call is None:
+                    first_call = current_call
+                if latest_calls:
+                    latest_calls.add_child(current_call)
+                latest_calls = current_call
             child_nodes = iter_child_nodes(node)
             child_nodes.reverse()
             if child_nodes is not None:
                 todo.extendleft(child_nodes)
-        return all_calls
+        return first_call
--- parser/classes/Call.py
+++ parser/classes/Call.py
@@ -3,11 +3,25 @@ from FuncCallVisitor import FuncCallVisitor
 
 class Call(ast.Call):
     
-    def __init__(self, node):
+    def __init__(self, node, parent):
         self.node = node
         self.name = self.get_name(node)
+        self.parent = parent
+        self.children = []
 
     def get_name(self, node):
         callvisitor = FuncCallVisitor()
         callvisitor.visit(node.func)
         return callvisitor.name
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def traverse(self):
+        cur_node = self;
+        calls = []
+        calls.append(cur_node.name)
+        while len(cur_node.children) is not 0:
+            cur_node = cur_node.children[0]
+            calls.append(cur_node.name)
+        return calls
