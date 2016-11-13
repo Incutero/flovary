f2e30827602d1da1222a515c80f07de8a8fe0022
Michael Shum <michaelmshum@live.com>
2016-11-13 02:02:16 -0500

    adding flatten method

--- parser/classes/ASTFuncDef.py
+++ parser/classes/ASTFuncDef.py
@@ -1,5 +1,5 @@
 import ast
-from collections import deque
+from collections import deque, Sequence
 from ast_helper import *
 from Call import Call 
 
@@ -12,7 +12,10 @@ class ASTFuncDef(object):
 
     def __str__(self):
         print self.name
- 
+
+    def flatten(self, l):
+        return sum(map(lambda x: x if isinstance(x, Sequence) else [x], l), [])
+
     # Walks through function and returns linkedlist of calls.
     def dfs_walk_get_calls(self, node):
         return self.dfs_walk_body([node])
@@ -34,16 +37,19 @@ class ASTFuncDef(object):
     def dfs_walk_body(self, todo):
         todo = deque(todo)
         latest_calls = [Call(None)]
-        first_call = None
+        first_call = None 
         while todo:
             node = todo.popleft()
             if isinstance(node, ast.If):
                 children = self.dfs_walk_ifs(node)
                 for child in children:
+#                    print 'name', child.name
                     for latest_call in latest_calls:
+#                        print latest_call.name
                         latest_call.add_child(child)
                         child.add_parent(latest_call)
                 latest_calls = [child.get_tails() for child in children]
+                latest_calls = self.flatten(latest_calls)
 #                print "if", latest_calls
                 child_nodes = None
             elif isinstance(node, ast.Call):
@@ -51,7 +57,7 @@ class ASTFuncDef(object):
                 if first_call is None:
                     first_call = current_call
 #                print latest_calls
-                for latest_call in latest_calls: 
+                for latest_call in latest_calls:
                     latest_call.add_child(current_call)
                     current_call.add_parent(latest_call)
                 latest_calls = [current_call]
